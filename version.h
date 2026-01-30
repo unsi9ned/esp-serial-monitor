71e8a7003c8d384eed21df3bd4af3efd49a8b89f
Andrey Osipov <osandr@tut.by>
2026-01-30 13:34:50 +0300

    build: update version to 0.2.0 and improve release workflow

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define APP_NAME           "ESP Serial Monitor+"
-#define APP_VERSION        "0.1.0"
+#define APP_VERSION        "0.2.0"
 #define APP_VERSION_MAJOR  0
 #define APP_VERSION_MINOR  1
 #define APP_VERSION_PATCH  0
